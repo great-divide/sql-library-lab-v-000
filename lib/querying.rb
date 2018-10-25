39a08187628d0eb5b35b0edc22975afc3079ea81
Brian Birdwell <bbirdwell@gmail.com>
2018-10-25 21:52:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, SUM(species) FROM characters GROUP BY species ORDER BY species DESC LIMIT 1"
+  "SELECT species, COUNT(characters) FROM characters GROUP BY species ORDER BY species DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
